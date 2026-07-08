8ed54a47603975d1bc96e2e4fc41e2b7d23b2d8f
Pethum Shyam <139078385+Pethumshyam6611@users.noreply.github.com>
2026-07-08 22:01:35 +0530

    Add team member listing endpoint

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authenticate = require('../middlewares/auth.middleware');
+const requireRole = require('../middlewares/role.middleware');
 const userController = require('../controllers/user.controller');
 
 const router = express.Router();
@@ -7,5 +8,6 @@ const router = express.Router();
 router.use(authenticate);
 
 router.get('/me', userController.me);
+router.get('/team-members', requireRole('manager'), userController.listTeamMembers);
 
 module.exports = router;
